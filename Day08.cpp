72261d02ae688689d8a13dde3e4c9e1189d001f3
Vishal-9643 <golavishal82@gmail.com>
2025-07-28 21:39:39 +0530

    Stock Buy & Sell II

--- Day08.cpp
+++ Day08.cpp
@@ -1,6 +1,6 @@
-// Day 8 : Stock Buy & Sell Max one transaction allowed
+// Day 8 : Stock Buy & Sell Max one transaction allowed.
 
-  // Approach is assuming first day as bestBuy and traversing the data if price greater  than bestBuy then calculate profit and if less make it bestbuy
+  // Approach is assuming first day as bestBuy and traversing the data if price greater  than bestBuy then calculate profit and if less make it bestbuy.
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -8,7 +8,7 @@ using namespace std;
 int maximumProfit(vector<int> &prices) {
         int n = prices.size();
         int MP =0;
-        int BB = prices[0]; // Best BUY
+        int BB = prices[0]; // Best BUY.
         for(int i=1;i<n;i++){
             if(BB<prices[i]){
                 int profit = prices[i]-BB;
