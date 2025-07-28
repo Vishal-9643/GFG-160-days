45b573239faf4665bf510fc158820e15c2f15910
Vishal-9643 <golavishal82@gmail.com>
2025-07-28 20:59:20 +0530

    Reverse Array

---
+++ Day3.cpp
@@ -0,0 +1,24 @@
+// Day 3 : Reverse a given array.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// function for reversing  array.
+void reverse(vector<int> & arr){
+    int n = arr.size();
+    int st = 0;
+    int end = n-1;
+    while(st<end){
+        swap(arr[st],arr[end]);
+        st++;
+        end--;
+    }
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4,5};
+    reverse(arr);
+    for(int num:arr){
+        cout << num << " ";
+    }
+}
