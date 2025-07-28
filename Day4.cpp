3637e2f84e8a7df9198826ee22178f5c64be0708
Vishal-9643 <golavishal82@gmail.com>
2025-07-28 21:00:00 +0530

    Rotate Array to Left by D steps

---
+++ Day4.cpp
@@ -0,0 +1,29 @@
+// Day 4 : Rotate the array to the left by D steps.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//function to rotate the array.
+void rotate(vector<int> & arr , int D){
+    int n = arr.size();
+    D = D%n; // handle the case when D>n;
+    int temp[n]; // creating temp arr.
+    int j =0;
+    for(int i =D;i<n;i++){
+        temp[j++] = arr[i];
+    }
+    for(int i =0;i<D;i++){
+        temp[j++] = arr[i];
+    }
+    for(int i =0;i<n;i++){
+        arr[i] = temp[i];
+    }
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4,5};
+    rotate(arr,2);
+    for(int num:arr){
+        cout << num << " ";
+    }
+}
