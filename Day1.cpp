738b0e900e62f77462e49abb6b9cc5f11ac1b591
Vishal-9643 <golavishal82@gmail.com>
2025-07-28 20:56:03 +0530

    Second Largest Element

---
+++ Day1.cpp
@@ -0,0 +1,28 @@
+// Day 1 : Finding the second largest element from an array.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Function for finding second largest element.
+int SL(vector<int> & arr){
+    int n = arr.size();
+    int largest = INT16_MIN;
+    int Slargest = INT16_MIN;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            Slargest = largest;
+            largest = arr[i];
+        }else if(arr[i]<largest && arr[i]>Slargest){
+            Slargest = arr[i];
+
+        }
+    }
+    return Slargest;
+}
+
+int main(){
+    vector<int> v = {12 , 10 , 16 , 89, 67};
+    cout << SL(v);
+    return 0;
+}
