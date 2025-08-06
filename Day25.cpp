4fd84abdb56749e0561d006b9995937b04604602
Vishal-9643 <golavishal82@gmail.com>
2025-08-06 22:07:05 +0530

    Merge Without Extra Space

--- Day25.cpp
+++ Day25.cpp
@@ -4,32 +4,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void mergeArrays(vector<int>& a, vector<int>& b) {
-        
-        int n = a.size();
-        int m = b.size();
-        
-        int p1 = n-1;
-        int p2 = 0;
-        
-        while(p1>=0 && p2<m && a[p1]>b[p2]){
-            swap(a[p1],b[p2]);
-            p1--;
-            p2++;
-        }
-        
-        sort(a.begin(),a.end());
-        sort(b.begin(),b.end());
+void mergeArrays(vector<int> &a, vector<int> &b)
+{
+
+    int n = a.size();
+    int m = b.size();
+
+    int p1 = n - 1;
+    int p2 = 0;
+
+    while (p1 >= 0 && p2 < m && a[p1] > b[p2])
+    {
+        swap(a[p1], b[p2]);
+        p1--;
+        p2++;
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
 }
 
-int main(){
+int main()
+{
     vector<int> A = {1, 5, 9, 10, 15, 20};
     vector<int> B = {2, 3, 8, 13};
-    mergeArrays(A,B);
-    for(int i : A){
+    mergeArrays(A, B);
+    for (int i : A)
+    {
         cout << i << " ";
     }
-    for(int i:B){
+    for (int i : B)
+    {
         cout << i << " ";
     }
 }
