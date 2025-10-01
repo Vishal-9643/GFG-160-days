bf9937d69167db834f23b63a29067f0d4fbd5fe1
Vishal-9643 <golavishal82@gmail.com>
2025-10-01 21:52:05 +0530

    Find Only Repetitive Element from 1 to n-1

---
+++ Day72.cpp
@@ -0,0 +1,25 @@
+// Day 72 : Find Only Repetitive Element from 1 to n-1.
+// Given an array arr[] of size n, filled with numbers from 1 to n-1 in random order. The array has only one repetitive element. Your task is to find the repetitive element.
+
+// Note: It is guaranteed that there is a repeating element present in the array.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDuplicate(vector<int> &arr)
+{
+    int n = arr.size();
+    int XOR = 0;
+    for (int i = 0; i < n; i++)
+    {
+        XOR = XOR ^ i ^ arr[i];
+    }
+
+    return XOR;
+}
+
+int main()
+{
+    vector<int> arr = {1, 3, 2, 3, 4};
+    cout << findDuplicate(arr);
+}
